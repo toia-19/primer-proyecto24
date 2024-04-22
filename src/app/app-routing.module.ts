ba8544d813ffb2ec2ab115ab6e58507c9162099f
toia-19 <vickingiediaz19@gmail.com>
2024-04-22 10:49:08 -0300

    creamos primeros módulos y configuramos las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // RUTA INICIAL / PRINCIPAL AL COMPONENTE
+  {
+    path:"",component: InicioComponent
+  },
+  // CARGA PEREZOSA -> RUTA AL MÓDULO INICIO
+  // loadChildren: Indica que será ruta hija del módulo raíz
+  // ()=>: Función flecha que importará la dirección del módulo
+  // .then: Promesa que nos devolerá un valor resuelto o rechazado
+  {
+    path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
