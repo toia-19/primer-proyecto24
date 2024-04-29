8a62c5f561ebf4a993f50585d6ba7a53a0878e3a
toia-19 <vickingiediaz19@gmail.com>
2024-04-29 10:54:26 -0300

    creación de primera interfaz y componente de material

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,16 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
+
+// VISTAS - PÁGINAS
 import { InicioComponent } from './pages/inicio/inicio.component';
 
+// COMPONENTES LOCALES
+import { CardComponent } from './components/card/card.component';
+
+// COMPONENTES DE MATERIAL
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    InicioComponent
+    InicioComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
-    InicioRoutingModule
+    InicioRoutingModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class InicioModule { }
