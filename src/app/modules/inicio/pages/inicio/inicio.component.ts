fcaeedff4867454f263f5c13573a7370c882dbf8
toia-19 <vickingiediaz19@gmail.com>
2024-05-06 09:23:22 -0300

    configuraciones en la interfaz; uso de directivas e interpolación

--- src/app/modules/inicio/pages/inicio/inicio.component.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-// IMPORTAMOS INTERFAZ
-import { Animal } from 'src/app/models/animal';
 
 @Component({
   selector: 'app-inicio',
@@ -8,35 +6,5 @@ import { Animal } from 'src/app/models/animal';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-  // PROPIEDAD PÚBLICA (TIPO: ARRAY)
-  public info: Animal[];
-  
-  constructor(){
-    this.info = [
-      {
-        id: "",
-        nombre: "Chanchito",
-        raza: "Cerdo",
-        edad: 5,
-        imagen: "https://i1.sndcdn.com/artworks-000190784980-i4qoly-t500x500.jpg",
-        alt: "Chanchito"
-      },
-      {
-        id: "",
-        nombre: "Perro",
-        raza: "Border Collie",
-        edad: 6,
-        imagen: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1371102402-645cff2ca5a62.jpg?crop=0.665xw:1.00xh;0.224xw,0&resize=1200:*",
-        alt: "Border Collie"
-      },
-      {
-        id: "",
-        nombre: "Pepe",
-        raza: "Gato",
-        edad: 2,
-        imagen: "https://i0.wp.com/puppis.blog/wp-content/uploads/2022/02/abc-cuidado-de-los-gatos-min.jpg?resize=521%2C346&ssl=1",
-        alt: "Gatito"
-      }
-    ]
-  }
+ 
 }
