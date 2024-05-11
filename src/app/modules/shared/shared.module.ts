601ab021e026b3db9099f98908a5052215196000
toia-19 <vickingiediaz19@gmail.com>
2024-05-10 23:00:42 -0300

    actualizando proyecto base

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
