57222ce862af35d25f0b512cac38166835310193
toia-19 <vickingiediaz19@gmail.com>
2024-05-27 10:53:16 -0300

    creando interfaz de usuario

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    uid: string | any; // atributos del tipo "any" reciben vacíos o indefinidos
+    nombre: string;
+    apellido: string;
+    email: string;
+    password: string;
+}
--- src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./iniciosesion.component.css']
 })
 export class IniciosesionComponent {
-
+  hide = true;
 }
