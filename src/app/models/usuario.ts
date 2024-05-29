e0d411aca847a6422e3922bd95d08648a1590222
toia-19 <vickingiediaz19@gmail.com>
2024-05-29 10:03:25 -0300

    solucionamos el error en el registro y creamos una nueva interfaz llamada 'usuario'

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    uid: string | any; // atributos tipo any = reciben valores indefinidos
+    nombre: string;
+    apellido: string;
+    email: string;
+    password: string;
+}
