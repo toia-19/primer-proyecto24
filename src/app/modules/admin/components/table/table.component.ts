aa4f6c0f4dfe706134bc94875b16bd97e1909d34
toia-19 <vickingiediaz19@gmail.com>
2024-08-05 10:58:28 -0300

    Creación de método 'agregarProducto' desde table.ts y creación de modal desde el html

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -19,12 +19,36 @@ export class TableComponent {
    */
   producto = new FormGroup({
     nombre: new FormControl('', Validators.required),
-    precio: new FormControl(0,Validators.required),
+    precio: new FormControl(0, Validators.required),
     descripcion: new FormControl('', Validators.required),
     categoria: new FormControl('', Validators.required),
     imagen: new FormControl('', Validators.required),
     alt: new FormControl('', Validators.required)
   })
 
-  constructor(public servicioCrud: CrudService){}
+  constructor(public servicioCrud: CrudService) { }
+
+  ngOnInit(): void { }
+
+  async agregarProducto() {
+    if (this.producto.valid) {
+      let nuevoProducto: Producto = {
+        idProducto: '',
+        nombre: this.producto.value.nombre!,
+        precio: this.producto.value.precio!,
+        descripcion: this.producto.value.descripcion!,
+        categoria: this.producto.value.categoria!,
+        imagen: this.producto.value.imagen!,
+        alt: this.producto.value.alt!
+      }
+
+      await this.servicioCrud.crearProducto(nuevoProducto)
+        .then(producto => {
+          alert("Ha agregado un nuevo producto con éxito.");
+        })
+        .catch(error => {
+          alert("Ha ocurrido un error al cargar un producto.");
+        })
+    }
+  }
 }
