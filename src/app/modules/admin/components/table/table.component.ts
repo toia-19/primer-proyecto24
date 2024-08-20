2097c8f148fd149a18bc410f751cce4fb2e0bc92
toia-19 <vickingiediaz19@gmail.com>
2024-08-20 10:57:53 -0300

    Vinculación de modal para tomar datos de un producto seleccionado; Modal para editar productos funcionando

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -54,13 +54,19 @@ export class TableComponent {
       await this.servicioCrud.crearProducto(nuevoProducto)
         .then(producto => {
           alert("Ha agregado un nuevo producto con éxito.");
+
+          // Resetea el formulario y las casillas quedan vacías
+          this.producto.reset();
         })
         .catch(error => {
           alert("Ha ocurrido un error al cargar un producto.");
+
+          this.producto.reset();
         })
     }
   }
 
+  // ELIMINAR PRODUCTOS
   // función vinculada al modal y el botón de la tabla
   mostrarBorrar(productoSeleccionado: Producto){
     this.modalVisibleProducto = true;
@@ -75,6 +81,49 @@ export class TableComponent {
     })
     .catch(error => {
       alert("Ha ocurrido un error al eliminar un producto: \n"+error);
+    })
+  }
+
+  // EDITAR PRODUCTOS
+  // Se envía y llama al momento que tocamos botón "Editar" de la tabla
+  mostrarEditar(productoSeleccionado: Producto){
+    this.productoSeleccionado = productoSeleccionado;
+    /*
+      Toma los valores del producto seleccionado y los va a
+      autocompletar en el formulario del modal (menos el ID)
+    */
+    this.producto.setValue({
+      nombre: productoSeleccionado.nombre,
+      precio: productoSeleccionado.precio,
+      descripcion: productoSeleccionado.descripcion,
+      categoria: productoSeleccionado.categoria,
+      imagen: productoSeleccionado.imagen,
+      alt: productoSeleccionado.alt
+    })
+  }
+
+  // VINCULA A BOTÓN "editarProducto" del modal de "Editar"
+  editarProducto(){
+    let datos: Producto = {
+      // Solo idProducto no se modifica por el usuario
+      idProducto: this.productoSeleccionado.idProducto,
+      /* Los demás atributos reciben nueva información/ 
+      valor desde el formulario */
+      nombre: this.producto.value.nombre!,
+      precio: this.producto.value.precio!,
+      descripcion: this.producto.value.descripcion!,
+      categoria: this.producto.value.categoria!,
+      imagen: this.producto.value.imagen!,
+      alt: this.producto.value.alt!
+    }
+
+    // Enviamos al método el id del producto seleccionado y los datos actualizados
+    this.servicioCrud.modificarProducto(this.productoSeleccionado.idProducto, datos)
+    .then(producto => {
+      alert("El producto se ha modificado con éxito.");
+    })
+    .catch(error => {
+      alert("Hubo un problema al modificar el producto: \n"+error);
     })
   }
 }
